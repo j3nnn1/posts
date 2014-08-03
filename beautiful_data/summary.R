52b98145e3995656b78715a5f45f8002900c0e21
j3nnn1 <jcmm986@gmail.com>
2014-08-03 19:31:08 -0300

    updating blog

--- beautiful_data/summary.R
+++ beautiful_data/summary.R
@@ -1,4 +1,23 @@
 
+
+url='http://samarium.wikimedia.org/campaign-vs-amount.csv';
+wiki = read.delim(url,sep=",");
+plot(wiki);
+
+
 wiki = read.table('campaign-vs-amount.csv', sep=',', header=T)
+head(wiki)
+
+wiki$stop_date = strptime(wiki$stop_date, format="%Y-%m-%d %H:%M:%S")
+wiki$medium = as.factor(wiki$medium)
+wiki$campaign = as.factor(wiki$campaign)
+wiki$count = as.numeric(wiki$count)
+wiki$start_date = strptime(wiki$start_date, format="%Y-%m-%d %H:%M:%S")
+ hist(wiki$sum)
+ hist(wiki$avg)
+ hist(wiki$count)
+ hist(wiki$usdmax)
+
+
 
 
